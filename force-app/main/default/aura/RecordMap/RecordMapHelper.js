89b6aa3096a581476b3c555d61a76f740e2785f2
Michael Dailey <michael.dailey@provartesting.com>
2022-10-11 09:49:16 -0500

    Reset Dreamhouse App

---
+++ force-app/main/default/aura/RecordMap/RecordMapHelper.js
@@ -0,0 +1,16 @@
+({
+    reloadRecord: function (component) {
+        var service = component.find('service');
+        service.reloadRecord(false, function () {
+            var sObject = component.get('v.sObject');
+            component.set('v.mapMarkers', [
+                {
+                    location: {
+                        Latitude: sObject[component.get('v.latField')],
+                        Longitude: sObject[component.get('v.longField')]
+                    }
+                }
+            ]);
+        });
+    }
+});
---
+++ force-app/main/default/aura/SmartHome/SmartHomeController.js
@@ -0,0 +1,3 @@
+({
+    myAction: function () {}
+});
